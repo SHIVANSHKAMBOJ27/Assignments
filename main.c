c3f248becf5a924456704de8332c1064ffa7ff06
SHIVANSHKAMBOJ27 <149481298+SHIVANSHKAMBOJ27@users.noreply.github.com>
2023-12-07 14:09:05 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TestProgram
+//
+//  Created by Shivansh Kamboj on 17/11/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
